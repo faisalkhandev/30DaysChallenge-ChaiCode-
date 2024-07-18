d9e114a634d9a78663a4dc417e8346f9de9089c7
faisalKhan <faisaldev404@gmail.com>
2024-07-18 10:31:47 +0500

    Day 2 done

---
+++ Day2.js
@@ -0,0 +1,24 @@
+const lumber1 = 10;
+const lumber2 = 10;
+
+const result = lumber1 + lumber2
+
+console.log(result)
+
+
+//! do same for addition, subtraction, multiplication, 4
+
+
+//* find the remainder 
+const divide1 = 12
+const divide2 = 5
+const res = divide1 % divide2
+console.log(res)
+
+
+
+const numberTest = 10;
+
+const storedResult = numberTest > 0 ? "Positive Lumber" : "Negative Lumber"
+
+console.log(storedResult)
